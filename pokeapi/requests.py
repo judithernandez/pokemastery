40304fff29e7792a1b203b3da13690affacb31db
Judith <judith@earthpulse.ai>
2023-08-21 15:34:14 +0200

    04_nb completed and working better

--- pokeapi/requests.py
+++ pokeapi/requests.py
@@ -1,4 +1,6 @@
 import requests
+from tqdm import tqdm
+import time
 
 def get_pokemon_data(pokemon_id):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
@@ -33,3 +35,18 @@ def get_pokemon_list():
         return data['results']
     else:
         return None
+    
+def get_pokemon_types(pokemon_id):
+    data = get_pokemon_data(pokemon_id)
+    types = [entry['type']['name'] for entry in data['types']]
+    return types
+    
+def get_pokemon_list_with_types():
+    pokemon_list_with_types = []
+    pokemon_list = get_pokemon_list()
+    for pokemon in tqdm(pokemon_list, desc='Getting Pokemon Types', unit='pokemon'):
+        pokemon_id = pokemon['url'].split('/')[-2]
+        types = get_pokemon_types(pokemon_id)
+        pokemon_list_with_types.append({'id': pokemon_id, 'name': pokemon['name'], 'types': types})
+        time.sleep(0.1)
+    return pokemon_list_with_types
