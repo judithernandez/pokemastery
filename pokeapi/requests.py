78800d9fbdbf77dce9c5e7a77cf92953cac6c984
Judith <judith@earthpulse.ai>
2023-08-21 11:31:18 +0200

    pokeapi requests in a separate module

---
+++ pokeapi/requests.py
@@ -0,0 +1,35 @@
+import requests
+
+def get_pokemon_data(pokemon_id):
+    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+    
+def get_pokemon_name(pokemon_id):
+    data = get_pokemon_data(pokemon_id)
+    if data:
+        return data["name"]
+    else:
+        return None
+    
+def get_pokemon_type_1(pokemon_id):
+    data = get_pokemon_data(pokemon_id)
+    if data:
+        return data["types"][0]["type"]["name"]
+    else:
+        return None
+    
+def get_pokemon():
+    url = f"https://pokeapi.co/api/v2/pokemon/"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
